a43a4b24a4a9cb38f6917772e280bdc2c83e2eb6
alokito1st <05alankita@gmail.com>
2025-01-23 14:47:18 +0530

    Add files via upload

---
+++ counting 0's and 1's.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int arr[]={1,1,1,0,0,0,0,1,0,1,0,1,0,1,0};0.0..021 
+	int size=15;
+	int numzero=0;
+	int numone=0;
+	
+	for(int i=0;i<size;i++){
+		if(arr[i]==0){
+			numzero++;
+		}
+		if(arr[i]==1){
+			numone++;
+		}
+		
+	}
+	cout<<"no of zeros:"<<numzero<<endl;
+	cout<<"no of ones:"<<numone<<endl;
+	
+	return 0;
+}
